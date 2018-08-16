56d5df7fd823b67c1898308206a9eded82fa4d23
unknown <fredian.pc@gmail.com>
2018-08-16 15:01:23 +0700

    minor fix

--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -5,7 +5,7 @@
         <div class="auto-container">
             <div class="clearfix">                
                 <div class="pull-left">
-                    <img class="logo-outer" src="{{config('website.setting.logo_light')}}" width="280">
+                    <img class="logo-outer img-responsive" src="{{config('website.setting.logo_light')}}">
                     <div class="logo"><a href="index-1.html"></a></div>
                 </div>
                 
@@ -89,8 +89,7 @@
             
         </div>
     </div>
-    <!--End Sticky Header-->
-    
+    <!--End Sticky Header-->    
 </header>
 <!--End Header Style Two -->
 
@@ -102,95 +101,25 @@
     </div>
     
     <!-- Hidden Bar Wrapper -->
-    <div class="hidden-bar-wrapper">
+    <div class="hidden-bar-wrapper">        
         <div class="logo">
-            <a href="index.html"></a>
+            <a href="index.html">
+            <img class="logo" src="{{config('website.setting.logo_small_light')}}" width="100">
+            </a>
         </div>
+        <br>
         <!-- .Side-menu -->
         <div class="side-menu">
             <!--navigation-->
             <ul class="navigation clearfix">
-                <li class="current dropdown"><a href="#">Home</a>
-                    <ul>
-                        <li><a href="index-1.html">Home 01</a></li>
-                        <li><a href="index-2.html">Home 02</a></li>
-                        <li><a href="index-3.html">Home 03</a></li>
-                        <li><a href="index-4.html">Home 04</a></li>
-                        <li><a href="index-5.html">Home 05</a></li>
-                        <li><a href="index-6.html">Home 06</a></li>
-                        <li><a href="index-7.html">Home 07</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown"><a href="#">Headers</a>
-                    <ul>
-                        <li><a href="header-1.html">Header 01</a></li>
-                        <li><a href="header-2.html">Header 02</a></li>
-                        <li><a href="header-3.html">Header 03</a></li>
-                    </ul>
-                </li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="gallery.html">gallery</a></li>
-                <li class="dropdown"><a href="#">Blog</a>
-                    <ul>
-                        <li><a href="blog-single.html">blog Single One</a></li>
-                        <li><a href="blog-single-2.html">Blog Single Two</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown"><a href="#">Shop</a>
-                    <ul>
-                        <li><a href="shop.html">Our Shop</a></li>
-                        <li><a href="shop-single.html">Shop Single</a></li>
-                    </ul>
-                </li>
-                <li><a href="contact.html">Contact</a></li>
+                <li class="current"><a href="{{route('front.home')}}">Home</a></li>
+                <li><a href="{{route('front.event')}}">Browse Event</a></li>
+                <li><a href="#">Organize</a></li>
+                <li><a href="{{route('front.news')}}">News</a></li>
              </ul>
         </div>
-        <!-- /.Side-menu -->
-        
-        <!--Options Box-->
-        <div class="options-box">
-            <!--Sidebar Search-->
-            <div class="sidebar-search">
-                <form method="post" action="contact.html">
-                    <div class="form-group">
-                        <input type="search" name="text" value="" placeholder="Search ..." required="">
-                        <button type="submit" class="theme-btn"><span class="fa fa-search"></span></button>
-                    </div>
-                </form>
-            </div>
-            
-            <!--Mobile Cart-->
-            <div class="mobile-cart">
-                <a href="shop-single.html" class="clearfix">
-                    <div class="pull-left">
-                        <div class="text">0 items 0.00$</div>
-                    </div>
-                    <div class="pull-right">
-                        <span class="icon fa fa-shopping-cart"></span>
-                    </div>
-                </a>
-            </div>
-            
-            <!--Language Dropdown-->
-            <div class="language dropdown"><a class="btn btn-default dropdown-toggle" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" href="#"> English <span class="icon fa fa-angle-down"></span></a>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <li><a href="#">English</a></li>
-                    <li><a href="#">German</a></li>
-                    <li><a href="#">Arabic</a></li>
-                    <li><a href="#">Hindi</a></li>
-                </ul>
-            </div>
-            
-            <!--Social Links-->
-            <ul class="social-links clearfix">
-                <li><a href="#"><span class="fa fa-facebook-f"></span></a></li>
-                <li><a href="#"><span class="fa fa-twitter"></span></a></li>
-                <li><a href="#"><span class="fa fa-instagram"></span></a></li>
-                <li><a href="#"><span class="fa fa-pinterest"></span></a></li>                    
-            </ul>
-            
-        </div>
-        
+        <!-- /.Side-menu -->       
+       
     </div><!-- / Hidden Bar Wrapper -->
     
 </section>

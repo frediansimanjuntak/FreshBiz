566aae4b81a8529d3bf96b4cae42ee73c8e256e0
unknown <fredian.pc@gmail.com>
2018-08-27 11:12:39 +0700

    minor fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,14 +16,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        $mail_setting = EmailSettings::first();
-        $website_setting = WebsiteSettings::first();
-        if ($mail_setting) {
-            config(['services.mandrill.secret' => $mail_setting->mandrill_key, 'mail.from.address' => $mail_setting->from_email, 'mail.from.name' => $mail_setting->from_name, 'mail.feedback_to.address' => $mail_setting->feedback_email_to]);
-        }
-        if ($website_setting) {
-            config(['website.setting.name' => $website_setting->name, 'website.setting.logo_light' => $website_setting->logo_light, 'website.setting.logo_dark' => $website_setting->logo_dark, 'website.setting.logo_small_light' => $website_setting->logo_small_light, 'website.setting.logo_small_dark' => $website_setting->logo_small_dark,'website.setting.meta_description' => $website_setting->meta]);
-        }
+        // $mail_setting = EmailSettings::first();
+        // $website_setting = WebsiteSettings::first();
+        // if ($mail_setting) {
+        //     config(['services.mandrill.secret' => $mail_setting->mandrill_key, 'mail.from.address' => $mail_setting->from_email, 'mail.from.name' => $mail_setting->from_name, 'mail.feedback_to.address' => $mail_setting->feedback_email_to]);
+        // }
+        // if ($website_setting) {
+        //     config(['website.setting.name' => $website_setting->name, 'website.setting.logo_light' => $website_setting->logo_light, 'website.setting.logo_dark' => $website_setting->logo_dark, 'website.setting.logo_small_light' => $website_setting->logo_small_light, 'website.setting.logo_small_dark' => $website_setting->logo_small_dark,'website.setting.meta_description' => $website_setting->meta]);
+        // }
     }
 
     /**
